64f1b8966c3542d5506b20d495bfacb0d50bb621
Murphy McMahon <pandeiro@gmail.com>
2014-07-15 21:39:05 -0300

    add printout to build

--- src/leiningen/new/dog/make-dist.bash
+++ src/leiningen/new/dog/make-dist.bash
@@ -3,11 +3,20 @@
 lesscss=$(which lessc)
 
 if [ -z "$lesscss" ]; then
+    echo
     echo "LessCSS compiler not found; skipping Less compilation"
+    echo
 else
+    echo
     echo "Compiling and minifying Less..."
+    echo
+
     lessc -x app/styles/main.less > dist/styles.css
 fi
+
+echo
+echo "Compiling ClojureScript..."
+echo
 
 ENV=production lein cljsbuild once production
 
