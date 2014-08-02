898600cf2ad83c368c325324a9d762ec58e2823a
Murphy McMahon <pandeiro@gmail.com>
2014-08-02 11:23:06 -0300

    add missing dep

--- src/leiningen/new/dog/project.clj
+++ src/leiningen/new/dog/project.clj
@@ -4,6 +4,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [ring/ring-core "1.2.2"]
                  [ring-refresh "0.1.1"]
+                 [enlive "1.1.5"]
                  [com.googlecode.htmlcompressor/htmlcompressor "1.4"]
                  [org.clojure/clojurescript "0.0-2268"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
