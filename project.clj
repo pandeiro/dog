ce6d84daa7a7f5f9bb245a3c43f8906b10a2dcdb
Murphy McMahon <pandeiro@gmail.com>
2014-08-09 12:30:28 -0300

    bump ver

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dog/lein-template "0.5.0"
+(defproject dog/lein-template "0.5.1"
   :description "A template for generating a single-page-app skeleton
 that uses ClojureScript, including core.async and reagent, and the CSS
 pre-processor LessCSS.
