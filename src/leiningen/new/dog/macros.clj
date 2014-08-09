24d216457301131b54d3203966390ccdf7b1b5ae
Murphy McMahon <pandeiro@gmail.com>
2014-08-09 12:35:56 -0300

    check config file exists

--- src/leiningen/new/dog/macros.clj
+++ src/leiningen/new/dog/macros.clj
@@ -1,15 +1,19 @@
 (ns {{name}}.macros
-  (:require [clojure.edn :as edn]))
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]))
 
 (defmacro resolve-config
   "Allows for a static configuration file with, eg
 different URLs for use with development and production
 environment services."
   []
-  (let [config (edn/read-string (slurp "config.edn"))
-        env    (System/getenv "ENV")]
+  (let [cfg-file (io/file "config.edn")
+        config   (if (.exists cfg-file)
+                   (edn/read-string (slurp cfg-file))
+                   {})
+        env      (System/getenv "ENV")]
     (if (= env "production")
-      (:production config)
-      (:development config))))
+      (:production config {})
+      (:development config {}))))
 
 
