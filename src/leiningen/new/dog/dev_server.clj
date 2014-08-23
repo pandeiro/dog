952dafaec8eea58fc8ad426138b485c7123338db
Murphy McMahon <pandeiro@gmail.com>
2014-08-23 19:14:25 -0300

    fix base handler

--- src/leiningen/new/dog/dev_server.clj
+++ src/leiningen/new/dog/dev_server.clj
@@ -1,14 +1,16 @@
 (ns {{name}}.dev.server
-  (:require [ring.middleware.file      :as file]
-            [ring.middleware.file-info :as file-util]
-            [ring.middleware.refresh   :as refresh]))
+  (:require
+   [ring.util.response        :as resp-util]
+   [ring.middleware.file-info :as file-util]
+   [ring.middleware.refresh   :as refresh]))
 
-(defn base [handler]
-  (fn [req] (handler req)))
+(defn base [req]
+  (let [resp (or (resp-util/file-response (:uri req) {:root "app"})
+                 (resp-util/not-found "Not found!"))]
+    resp))
 
 (def handler
   (-> base
-    (file/wrap-file "app")
     file-util/wrap-file-info
     (refresh/wrap-refresh ["app"])))
 
