4270cf309c30f2f01f7ddddeefd24149d2db2cfc
Murphy McMahon <pandeiro@gmail.com>
2014-09-21 13:58:20 -0300

    add port arg

--- src/leiningen/new/dog/dev-server.bash
+++ src/leiningen/new/dog/dev-server.bash
@@ -2,13 +2,15 @@
 
 trap "kill 0" SIGINT
 
-echo "Starting the dev-server, just a second..."
+port=$1 || 3000
+
+echo "Starting the dev-server on port ${port}, just a second..."
 
 # Run inside subshells so they fork but still die on Ctrl-C
 (
 lein trampoline cljsbuild auto dev &
 )
 
-lein trampoline ring server-headless
+lein trampoline ring server-headless $port
 
 
