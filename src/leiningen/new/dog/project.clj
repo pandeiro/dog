82fa3bbab41d105b3ab812e2e8ac13fd7a786980
Murphy McMahon <pandeiro@gmail.com>
2014-10-11 12:45:20 -0300

    bump ring

--- src/leiningen/new/dog/project.clj
+++ src/leiningen/new/dog/project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [ring/ring-core "1.2.2"]
+                 [ring/ring-core "1.3.1"]
                  [ring-refresh "0.1.1"]
                  [enlive "1.1.5"]
                  [com.googlecode.htmlcompressor/htmlcompressor "1.4"]
